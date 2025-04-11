3e88129c490ff38852a19355b62f7651383b1291
davidmathias <thestardavid@gmail.com>
2025-04-12 02:04:06 +0530

    Working server copy

--- src/main/java/com/example/service/ServiceApplication.java
+++ src/main/java/com/example/service/ServiceApplication.java
@@ -1,13 +1,34 @@
 package com.example.service;
 
+import org.springframework.ai.tool.ToolCallbackProvider;
+import org.springframework.ai.tool.annotation.Tool;
+import org.springframework.ai.tool.method.MethodToolCallbackProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import java.io.File;
 
 @SpringBootApplication
 public class ServiceApplication {
-
+	public static final String FOLDER="/home/davidmathias/work/mcp/mcp-client-demo-01/user-context";
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceApplication.class, args);
 	}
 
+	@Bean
+	ToolCallbackProvider toolCallbackProvider(Tools tools){
+		return MethodToolCallbackProvider.builder().toolObjects(tools).build();
+	}
+
+
+}
+
+@Component
+class Tools{
+	@Tool(description = "List all files in the user context folder")
+	String[] fileTools() {
+		return new File(ServiceApplication.FOLDER).list();
+	}
 }
